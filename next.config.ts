0cfad5d2bb029cf3e9e1893ff2040158b7048984
Ajeet Singh <2016appsingh@gmail.com>
2025-02-23 10:59:31 +0530

    canges

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/download',
-        destination: '/app/api/download/route.js'
-      }
-    ];
-  }
+  
     
 };
 
